a0cc9e418f1e82571bbf3fe184eea4560f783b07
Astrotom3000 <Astrotom3000@gmail.com>
2014-02-27 16:39:53 -0500

    Added mockup to search view.

--- app/views/searchresults.blade.php
+++ app/views/searchresults.blade.php
@@ -8,6 +8,25 @@
 			{{ Form::hidden('searchphrase', $searchword, array('id' => 'query')) }}
 		{{ Form::close() }}
 
+	<div class="container">
+		<h3>Filter Results</h3>
+		{{ Form::open() }}
+		<div class="form-group input-form">
+			{{ Form::label('filtermovies', 'Movies') }}
+			{{ Form::checkbox('movies', '$filtermovies', true)}}
+			<label class="field-icon fa fa-envelope"></label>
+			{{ Form::label('filtershows', 'TV Shows') }}
+			{{ Form::checkbox('shows', '$filtershows', true)}}
+			<label class="field-icon fa fa-envelope"></label>
+			{{ Form::label('filteractors', 'Actors') }}
+			{{ Form::checkbox('actors', '$filteractors', true)}}
+			<label class="field-icon fa fa-envelope"></label>
+			{{ Form::label('filterdirectors', 'Directors') }}
+			{{ Form::checkbox('directors', '$filterdirectors', true)}}
+			<label class="field-icon fa fa-envelope"></label>
+		</div>
+		{{ Form::close() }}
+	</div>
 	<div class='row' id="display-results">
 	</div>
 	
@@ -15,7 +34,7 @@
 </div>
 @endsection
 <!--Request to grab our TMDB search results-->
-@section( 'scripts' )
+@section('scripts')
 <script>
 	$(function() {
 	    var query = $('input#query').val();
